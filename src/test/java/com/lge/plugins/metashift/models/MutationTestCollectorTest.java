dac86b78445140921bc661ee6b82127db98ec9ef
Sung Gon Kim <sunggon82.kim@lge.com>
2021-03-21 19:27:43 +0900

    #55 Adds the MutationTestCollector class

---
+++ src/test/java/com/lge/plugins/metashift/models/MutationTestCollectorTest.java
@@ -0,0 +1,150 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 LG Electronics, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.lge.plugins.metashift.models;
+
+import java.util.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * Unit tests for the MutationTestCollector class.
+ *
+ * @author Sung Gon Kim
+ */
+public class MutationTestCollectorTest {
+  private MutationTestCollector collector;
+  private MutationTestSet set;
+  private Recipe recipe;
+  private RecipeSet recipes;
+
+  private void assertValues(int denominator, int numerator, float ratio) {
+    assertEquals(denominator, collector.getDenominator());
+    assertEquals(numerator, collector.getNumerator());
+    assertEquals(ratio, collector.getRatio(), 0.1f);
+  }
+
+  @Before
+  public void setUp() throws Exception {
+    collector = new MutationTestCollector(KilledMutationTestData.class);
+    set = new MutationTestSet();
+    recipe = new Recipe("A-B-C");
+    recipes = new RecipeSet();
+  }
+
+  @Test
+  public void testInitialState() throws Exception {
+    assertValues(0, 0, 0.0f);
+  }
+
+  @Test
+  public void testEmptySet() throws Exception {
+    set.accept(collector);
+    assertValues(0, 0, 0.0f);
+  }
+
+  @Test
+  public void testSetWithoutMatched() throws Exception {
+    set.add(new SurvivedMutationTestData("A", "a.file", "C", "f()", 1, "AOR", "TC"));
+    set.accept(collector);
+    assertValues(1, 0, 0.0f);
+  }
+
+  @Test
+  public void testSetWithMatched() throws Exception {
+    set.add(new KilledMutationTestData("A", "a.file", "C", "f()", 2, "AOR", "TC"));
+    set.accept(collector);
+    assertValues(1, 1, 1.0f);
+  }
+
+  @Test
+  public void testSetWithCompoundData() throws Exception {
+    set.add(new SurvivedMutationTestData("A", "a.file", "C", "f()", 1, "AOR", "TC"));
+    set.add(new KilledMutationTestData("A", "a.file", "C", "f()", 2, "AOR", "TC"));
+    set.accept(collector);
+    assertValues(2, 1, 0.5f);
+  }
+
+  @Test
+  public void testMultipleSets() throws Exception {
+    List<MutationTestSet> group = new ArrayList<>();
+
+    set = new MutationTestSet();
+    set.add(new SurvivedMutationTestData("A", "a.file", "C", "f()", 1, "AOR", "TC"));
+    group.add(set);
+
+    set = new MutationTestSet();
+    set.add(new KilledMutationTestData("A", "a.file", "C", "f()", 2, "AOR", "TC"));
+    group.add(set);
+
+    group.forEach(o -> o.accept(collector));
+    assertValues(2, 1, 0.5f);
+  }
+
+  @Test
+  public void testEmptyRecipe() throws Exception {
+    recipe.accept(collector);
+    assertValues(0, 0, 0.0f);
+  }
+
+  @Test
+  public void testRecipeWithoutMatched() throws Exception {
+    set.add(new SurvivedMutationTestData("A", "a.file", "C", "f()", 1, "AOR", "TC"));
+    recipe.set(set);
+    recipe.accept(collector);
+    assertValues(1, 0, 0.0f);
+  }
+
+  @Test
+  public void testRecipeWithMatched() throws Exception {
+    set.add(new KilledMutationTestData("A", "a.file", "C", "f()", 2, "AOR", "TC"));
+    recipe.set(set);
+    recipe.accept(collector);
+    assertValues(1, 1, 1.0f);
+  }
+
+  @Test
+  public void testEmptyRecipeSet() throws Exception {
+    recipes.accept(collector);
+    assertValues(0, 0, 0.0f);
+  }
+
+  @Test
+  public void testRecipeSetWithCompoundData() throws Exception {
+    set = new MutationTestSet();
+    set.add(new SurvivedMutationTestData("A", "a.file", "C", "f()", 1, "AOR", "TC"));
+    recipe = new Recipe("A-1.0.0-r0");
+    recipe.set(set);
+    recipes.add(recipe);
+
+    set = new MutationTestSet();
+    set.add(new KilledMutationTestData("A", "a.file", "C", "f()", 2, "AOR", "TC"));
+    recipe = new Recipe("B-1.0.0-r0");
+    recipe.set(set);
+    recipes.add(recipe);
+
+    recipes.accept(collector);
+    assertValues(2, 1, 0.5f);
+  }
+}
