bffd2c36e021e2e83c132557e721aa492ea20002
Sung Gon Kim <sunggon82.kim@lge.com>
2021-04-04 18:00:17 +0900

    #64 Adds TestList.create() factory method

--- src/main/java/com/lge/plugins/metashift/models/TestList.java
+++ src/main/java/com/lge/plugins/metashift/models/TestList.java
@@ -25,6 +25,20 @@
 package com.lge.plugins.metashift.models;
 
 import com.lge.plugins.metashift.metrics.Visitable;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.apache.commons.io.FileUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  * Represents a set of TestData objects.
@@ -32,6 +46,127 @@ import com.lge.plugins.metashift.metrics.Visitable;
  * @author Sung Gon Kim
  */
 public final class TestList extends DataList<TestData> {
+
+  /**
+   * Create a set of objects by parsing a report file from the given path.
+   *
+   * @param path to the report directory
+   * @return a list of objects
+   * @throws IllegalArgumentException if failed to parse report files
+   */
+  public static TestList create(final File path) throws IllegalArgumentException {
+    TestList list = new TestList();
+    String recipe = path.getName();
+    String[] extensions = {"xml"};
+    File directory = new File(path, "test");
+    if (!directory.exists()) {
+      return list;
+    }
+    Collection<File> files = FileUtils.listFiles(directory, extensions, true);
+    if (files.isEmpty()) {
+      return list;
+    }
+    for (File file : files) {
+      try {
+        list.addAll(parseFile(recipe, file));
+      } catch (IOException e) {
+        e.printStackTrace();
+      } catch (ParserConfigurationException | SAXException e) {
+        e.printStackTrace();
+        throw new IllegalArgumentException("Failed to parse: " + file);
+      }
+    }
+    Collections.sort(list);
+    return list;
+  }
+
+  /**
+   * parse the report file to create list of test data.
+   *
+   * @param recipe name
+   * @param file   report file
+   * @return a list of test data objects
+   * @throws ParserConfigurationException if failed to parse the xml files
+   * @throws IOException                  if failed to parse the xml files
+   * @throws SAXException                 if failed to parse the xml files
+   */
+  private static Collection<? extends TestData> parseFile(final String recipe, final File file)
+      throws ParserConfigurationException, IOException, SAXException {
+    Collection<TestData> list = new ArrayList<>();
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    DocumentBuilder builder = factory.newDocumentBuilder();
+    Document document = builder.parse(file);
+    document.getDocumentElement().normalize();
+    NodeList nodes = document.getElementsByTagName("testsuite");
+    for (int i = 0; i < nodes.getLength(); i++) {
+      Node node = nodes.item(i);
+      if (node.getNodeType() == Node.ELEMENT_NODE) {
+        Element element = (Element) node;
+        String suite = element.getAttribute("name");
+        list.addAll(createTestDataList(recipe, suite, element));
+      }
+    }
+    return list;
+  }
+
+  /**
+   * Create a list of test data.
+   *
+   * @param recipe  name
+   * @param suite   name
+   * @param element to parse
+   * @return a list of test data
+   */
+  private static Collection<? extends TestData> createTestDataList(final String recipe,
+      final String suite, final Element element) {
+    Collection<TestData> list = new ArrayList<>();
+    NodeList nodes = element.getElementsByTagName("testcase");
+    for (int i = 0; i < nodes.getLength(); i++) {
+      Node node = nodes.item(i);
+      if (node.getNodeType() == Node.ELEMENT_NODE) {
+        list.add(createTestData(recipe, suite, (Element) node));
+      }
+    }
+    return list;
+  }
+
+  /**
+   * Creates the test data object.
+   *
+   * @param recipe  name
+   * @param suite   name
+   * @param element to parse
+   * @return a test data object
+   */
+  private static TestData createTestData(final String recipe, final String suite,
+      final Element element) {
+    String name = element.getAttribute("name");
+    if (!element.hasChildNodes()) {
+      return new PassedTestData(recipe, suite, name, "");
+    }
+    NodeList nodeList = element.getChildNodes();
+    for (int i = 0; i < nodeList.getLength(); i++) {
+      Node node = nodeList.item(i);
+      if (node.getNodeType() != Node.ELEMENT_NODE) {
+        continue;
+      }
+      String message = ((Element) node).getAttribute("message");
+      String status = ((Element) node).getTagName();
+      switch (status) {
+        case "failure":
+          return new FailedTestData(recipe, suite, name, message);
+        case "error":
+          return new ErrorTestData(recipe, suite, name, message);
+        case "skipped":
+          return new SkippedTestData(recipe, suite, name, message);
+        default:
+          throw new IllegalArgumentException(
+              String.format("Failed to parse: %s.%s.%s", recipe, suite, name));
+      }
+    }
+    throw new IllegalArgumentException(
+        String.format("Failed to parse: %s.%s.%s", recipe, suite, name));
+  }
 
   @Override
   public void accept(final Visitable visitor) {
--- src/test/java/com/lge/plugins/metashift/models/TestListTest.java
+++ src/test/java/com/lge/plugins/metashift/models/TestListTest.java
@@ -26,8 +26,21 @@ package com.lge.plugins.metashift.models;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 /**
  * Unit tests for the TestList class.
@@ -36,11 +49,31 @@ import org.junit.Test;
  */
 public class TestListTest {
 
+  @Rule
+  public final TemporaryFolder folder = new TemporaryFolder();
   private TestList objects;
 
   @Before
   public void setUp() {
     objects = new TestList();
+  }
+
+  private File createTempFile(String path, Collection<String> lines) throws Exception {
+    File directory = FileUtils.getFile(folder.getRoot(), FilenameUtils.getPath(path));
+    if (!directory.exists()) {
+      FileUtils.forceMkdir(directory);
+    }
+    File file = new File(directory, FilenameUtils.getName(path));
+    FileUtils.writeLines(file, lines);
+    return file;
+  }
+
+  private void assertValues(TestData object, String recipe, String suite, String name,
+      String message) {
+    assertEquals(recipe, object.getRecipe());
+    assertEquals(suite, object.getSuite());
+    assertEquals(name, object.getName());
+    assertEquals(message, object.getMessage());
   }
 
   @Test
@@ -56,5 +89,157 @@ public class TestListTest {
     objects.add(first);
     assertEquals(2, objects.size());
     assertEquals(first, objects.get(1));
+  }
+
+  @Test
+  public void testCreateWithUnknownPath() {
+    objects = TestList.create(new File(folder.getRoot(), "unknown"));
+    assertEquals(0, objects.size());
+  }
+
+  @Test
+  public void testCreateWithNoFile() throws Exception {
+    File file = folder.newFolder("report/A/test");
+    objects = TestList.create(file.getParentFile());
+    assertEquals(0, objects.size());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testCreateWithMalformedFile() throws Exception {
+    List<String> data = Arrays.asList(
+        "<testsuite",
+        "testsuite/>"
+    );
+    File file = createTempFile("report/A/test/1.xml", data);
+    objects = TestList.create(file.getParentFile().getParentFile());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testCreateWithEmptyFile() throws Exception {
+    List<String> data = Collections.singletonList("");
+    File file = createTempFile("report/B/test/1.xml", data);
+    objects = TestList.create(file.getParentFile().getParentFile());
+  }
+
+  @Test
+  public void testCreateWithEmptyData() throws Exception {
+    List<String> data = Arrays.asList(
+        "<testsuites>",
+        "</testsuites>"
+    );
+    File file = createTempFile("report/B/test/1.xml", data);
+    objects = TestList.create(file.getParentFile().getParentFile());
+  }
+
+  @Test
+  public void testCreateWithSingleData() throws Exception {
+    List<String> data = Arrays.asList(
+        "<testsuites>",
+        "  <testsuite name=\"A\">",
+        "    <testcase name=\"test1\"/>",
+        "  </testsuite>",
+        "</testsuites>"
+    );
+    File file = createTempFile("report/C/test/1.xml", data);
+
+    objects = TestList.create(file.getParentFile().getParentFile());
+    assertEquals(1, objects.size());
+
+    TestData object = objects.iterator().next();
+    assertValues(object, "C", "A", "test1", "");
+  }
+
+  @Test
+  public void testCreateWithMultipleData() throws Exception {
+    List<String> data = Arrays.asList(
+        "<testsuites>",
+        "  <testsuite name=\"A\">",
+        "    <testcase name=\"test1\"/>",
+        "  </testsuite>",
+        "  <testsuite name=\"B\">",
+        "    <testcase name=\"test2\">",
+        "      <failure message=\"failure\"/>",
+        "    </testcase>",
+        "  </testsuite>",
+        "  <testsuite name=\"C\">",
+        "    <testcase name=\"test3\">",
+        "      <error message=\"error\"/>",
+        "    </testcase>",
+        "  </testsuite>",
+        "  <testsuite name=\"D\">",
+        "    <testcase name=\"test4\">",
+        "      <skipped message=\"skipped\"/>",
+        "    </testcase>",
+        "  </testsuite>",
+        "</testsuites>"
+    );
+    File file = createTempFile("report/D/test/1.xml", data);
+
+    objects = TestList.create(file.getParentFile().getParentFile());
+    assertEquals(4, objects.size());
+
+    Iterator<TestData> iterator = objects.iterator();
+    assertValues(iterator.next(), "D", "A", "test1", "");
+    assertValues(iterator.next(), "D", "B", "test2", "failure");
+    assertValues(iterator.next(), "D", "C", "test3", "error");
+    assertValues(iterator.next(), "D", "D", "test4", "skipped");
+  }
+
+  @Test
+  public void testCreateWithMultipleFiles() throws Exception {
+    Map<String, List<String>> contents = new HashMap<>();
+    contents.put("report/D/test/1.xml",
+        Arrays.asList(
+            "<testsuites>",
+            "  <testsuite name=\"A\">",
+            "    <testcase name=\"test1\"/>",
+            "  </testsuite>",
+            "</testsuites>"
+        )
+    );
+    contents.put("report/D/test/2.xml",
+        Arrays.asList(
+            "<testsuites>",
+            "  <testsuite name=\"B\">",
+            "    <testcase name=\"test2\">",
+            "      <failure message=\"failure\"/>",
+            "    </testcase>",
+            "  </testsuite>",
+            "</testsuites>"
+        )
+    );
+    contents.put("report/D/test/3.xml",
+        Arrays.asList(
+            "<testsuites>",
+            "  <testsuite name=\"C\">",
+            "    <testcase name=\"test3\">",
+            "      <error message=\"error\"/>",
+            "    </testcase>",
+            "  </testsuite>",
+            "</testsuites>"
+        )
+    );
+    contents.put("report/D/test/4.xml",
+        Arrays.asList(
+            "<testsuites>",
+            "  <testsuite name=\"D\">",
+            "    <testcase name=\"test4\">",
+            "      <skipped message=\"skipped\"/>",
+            "    </testcase>",
+            "  </testsuite>",
+            "</testsuites>"
+        )
+    );
+    for (Map.Entry<String, List<String>> entry : contents.entrySet()) {
+      createTempFile(entry.getKey(), entry.getValue());
+    }
+    objects = TestList.create(FileUtils.getFile(folder.getRoot(), "report", "D"));
+    assertEquals(4, objects.size());
+
+    Iterator<TestData> iterator = objects.iterator();
+    assertValues(iterator.next(), "D", "A", "test1", "");
+    assertValues(iterator.next(), "D", "B", "test2", "failure");
+    assertValues(iterator.next(), "D", "C", "test3", "error");
+    assertValues(iterator.next(), "D", "D", "test4", "skipped");
   }
 }
